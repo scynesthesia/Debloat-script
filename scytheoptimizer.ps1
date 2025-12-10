21b70c26e6e96a43e989672f73cacdc716286bd9
scynesthesia <sanchez2674266@gmail.com>
2025-12-10 19:14:40 -0300

    Rename optimizer entrypoint and remove legacy naming

--- scytheoptimizer.ps1
+++
@@ -1,4 +0,0 @@
-# Deprecated entrypoint. Redirecting to scynesthesiaoptimizer.ps1
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$newPath = Join-Path $scriptDir 'scynesthesiaoptimizer.ps1'
-. $newPath
